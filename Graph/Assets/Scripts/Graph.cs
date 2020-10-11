efa6c5d0100471538a0e9513038e8416dd7e23d3
Ronald Pereira <ronaldpereira01@gmail.com>
2020-10-10 21:38:10 -0400

    added Multi Wave and Ripple graphs

--- Graph/Assets/Scripts/Graph.cs
+++ Graph/Assets/Scripts/Graph.cs
@@ -6,8 +6,11 @@ public class Graph : MonoBehaviour
 {
     [SerializeField]
 	Transform pointPrefab = default;
-    [SerializeField, Range(10, 100)]
+    [SerializeField, Range(10, 100)]	
 	int resolution = 10;
+
+	[SerializeField]
+	FunctionLibrary.FunctionName function = default;
 
 	Transform[] points;
 
@@ -29,11 +32,12 @@ public class Graph : MonoBehaviour
 
 	void Update () {
 		float time = Time.time;
+		FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
 		for (int i = 0; i < points.Length; i++)
 		{
 			Transform point = points[i];
 			Vector3 position = point.localPosition;
-			position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+			position.y = f(position.x, time);
 			point.localPosition = position;
 		}
 	}
