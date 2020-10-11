a5449c229aafd01873ba7d61de2c0353df54a0c1
Ronald Pereira <ronaldpereira01@gmail.com>
2020-10-11 09:34:53 -0400

    refactoring library to return Vector3 instead of floating values

--- Graph/Assets/Scripts/Graph.cs
+++ Graph/Assets/Scripts/Graph.cs
@@ -5,46 +5,47 @@ using UnityEngine;
 public class Graph : MonoBehaviour
 {
     [SerializeField]
-	Transform pointPrefab = default;
-    [SerializeField, Range(10, 100)]	
-	int resolution = 10;
+    Transform pointPrefab = default;
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
 
-	[SerializeField]
-	FunctionLibrary.FunctionName function = default;
+    [SerializeField]
+    FunctionLibrary.FunctionName function = default;
 
-	Transform[] points;
+    Transform[] points;
 
-    void Awake () {
+    void Awake()
+    {
         float step = 2f / resolution;
         var scale = Vector3.one * step;
-        Vector3 position = Vector3.zero;	
-		points = new Transform[resolution * resolution];	
+        points = new Transform[resolution * resolution];
 
-		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
-			if (x == resolution) {
-				x = 0;
-				z += 1;
-			}
-			Transform point = Instantiate(pointPrefab);
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            point.localScale = scale;
             point.SetParent(transform, false);
-			position.x = (x + 0.5f) * step - 1f;
-			position.z = (z + 0.5f) * step - 1f;
-			point.localPosition = position;
-			point.localScale = scale;
-			points[i] = point;
-		}
-	}
+            points[i] = point;
+        }
+    }
 
-	void Update () {
-		float time = Time.time;
-		FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
-		for (int i = 0; i < points.Length; i++)
-		{
-			Transform point = points[i];
-			Vector3 position = point.localPosition;
-			position.y = f(position.x, position.z, time);
-			point.localPosition = position;
-		}
-	}
-    
+    void Update()
+    {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
+        }
+    }
+
 }
